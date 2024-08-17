f7c6eff6e740899330b00365137fe5ef0affc629
berlaypott143 <aldrinjaylej@gmail.com>
2024-08-17 20:36:52 +0800

    2nd commit
    need to add logic to control result duplication and add limit per day

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import { quotes } from "./Quotes.js";
 
-console.log(quotes);
+let button = document.getElementById("btn");
+let container = document.getElementById("quote-container");
+
+button.addEventListener("click", function () {
+  container.innerHTML = "";
+  //getting a random index of imported quotes array
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  //console.log(quotes.length); ask how to access the content of the quotes length - chatgpt
+
+  //creating a div
+  let randomQuote = document.createElement("div");
+
+  //giving a name of the div
+  randomQuote.className = "quote";
+
+  //setting text content of div with the random selected quote
+  randomQuote.textContent = quotes[randomIndex];
+
+  container.appendChild(randomQuote);
+
+  console.log(container);
+});
